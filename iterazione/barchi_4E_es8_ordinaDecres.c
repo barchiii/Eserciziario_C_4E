6fdec35bd10292e19826c040aa552c70934e7e15
barchiii <145505905+barchiii@users.noreply.github.com>
2024-09-30 08:34:08 +0200

    Create barchi_4E_es8_ordinaDecres.c

---
+++ iterazione/barchi_4E_es8_ordinaDecres.c
@@ -0,0 +1,29 @@
+/* Dato N un numero intero positivo, generare e visualizzare in ordine
+decrescente i primi N numeri interi positivi. */
+
+#inlcude <stdio.h>
+
+int  main(int argc, char* argv[])
+{
+
+  int num, i = 0;
+
+  do
+    {
+      
+      printf("Inserisci un numero:\n");
+      scanf("%d", &num);
+      
+    } while (num <= 1);
+
+  i = n
+
+  do
+    {
+
+      printf("%d\t",i);
+      i--;
+    }
+
+    return 0;
+}
